8bc4555758788fab4ec03360ba25f94305b1c848
Joshua Yuuki Onizuka <yuuki0527@hotmail.com>
2022-01-24 15:34:09 -0300

    Alteração Componente

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,14 +4,14 @@ import {RouterModule, Routes} from '@angular/router';
 import { MuralVagasComponent } from '../mural-vagas/mural-vagas.component';
 import { PainelVagasComponent } from '../painel-vagas/painel-vagas.component';
 
-const rotas: Routes = [
+const routes: Routes = [
   {path: 'mural-vagas', component: MuralVagasComponent },
   {path: 'painel', component: PainelVagasComponent },
   {path:'',redirectTo:'/mural', pathMatch:'full'}
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(rotas)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
