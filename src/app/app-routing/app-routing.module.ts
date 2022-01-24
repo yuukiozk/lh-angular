35bfaa5482912dd9009ce4df50b82323c5afdebf
Joshua Yuuki Onizuka <yuuki0527@hotmail.com>
2022-01-24 15:12:06 -0300

    Start Angular

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import { MuralVagasComponent } from '../mural-vagas/mural-vagas.component';
+import { PainelVagasComponent } from '../painel-vagas/painel-vagas.component';
+
+const rotas: Routes = [
+  {path: 'mural-vagas', component: MuralVagasComponent },
+  {path: 'painel', component: PainelVagasComponent },
+  {path:'',redirectTo:'/mural', pathMatch:'full'}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(rotas)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
+import { RouterModule } from '@angular/router';
+import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuralVagasComponent,
+    PainelVagasComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
